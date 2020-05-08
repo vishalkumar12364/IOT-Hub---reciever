4b795e53713b39033f5b93a7daea0003f948cab6
Vishal Kumar <kumarvishal12364@gmail.com>
2020-05-08 21:39:29 +0530

    Add project files.

---
+++ IOT Hub reciever/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using Microsoft.Azure.Devices.Client;
+
+namespace IOT_Hub_reciever
+{
+    class Program
+    { 
+        static DeviceClient s_deviceClient;
+        static string connectionstring = "HostName=vishaliothub.azure-devices.net;DeviceId=VishalIotDevice;SharedAccessKey=kjzugZtDGYALkwnF/ILciH+9pai6RTLBmk/dQKJV6kg=";
+        static void Main(string[] args)
+        {
+            s_deviceClient = DeviceClient.CreateFromConnectionString(connectionstring);
+            RecieveC2dAsync();
+            Console.ReadLine();
+        }
+        private static async void RecieveC2dAsync()
+        {
+            Console.WriteLine("Message recieving from Cloud to device from service");
+            while (true)
+            {
+                Microsoft.Azure.Devices.Client.Message recievedmsg = await s_deviceClient.ReceiveAsync();
+                if (recievedmsg == null) continue;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Recieved message: {0}",
+                    Encoding.ASCII.GetString(recievedmsg.GetBytes()));
+                Console.ResetColor();
+
+                await s_deviceClient.CompleteAsync(recievedmsg);
+            }
+        }
+    }
+}
